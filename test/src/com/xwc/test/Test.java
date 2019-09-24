0fc084dde8223538b85d08bea235baf79a7eb935
perfect <39237223+perfectismman@users.noreply.github.com>
2019-09-24 22:23:31 +0800

    Update Test.java

--- test/src/com/xwc/test/Test.java
+++ test/src/com/xwc/test/Test.java
@@ -3,9 +3,6 @@ package com.xwc.test;
 public class Test {
    public static void main(String[] args) {
 	System.out.println("ÄãÊÇÖí");
-	System.out.println("¶Ô£¬ÄãÒ²ÊÇÖí");
-	   System.out.println("¶Ô£¬ÄãÒ²ÊÇÖí");
-	    System.out.println("¶Ô£¬ÄãÒ²ÊÇÖí"); 
-	   System.out.println("¶Ô£¬ÄãÒ²ÊÇÖí");
+	
 }
 } 
